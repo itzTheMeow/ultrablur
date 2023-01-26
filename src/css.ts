1bd0d40b0552a6c61abe6e1efc37324bd6f1e246
Meow <50887230+itzTheMeow@users.noreply.github.com>
2023-01-25 23:17:09 -0500

    add saturation

--- src/css.ts
+++ src/css.ts
@@ -5,9 +5,10 @@ import config from "./config";
  * Generate a CSS `background` value using radial-gradient.
  * @param color The foreground color to use.
  * @param background The background color to use.
+ * @param saturation Adds this value to the tinycolor mix percentage (0-100). Lower values blend toward the background.
  * @returns The CSS string for the `background` rule.
  */
-export function generateUltrablurCSS(color: string, background: string) {
+export function generateUltrablurCSS(color: string, background: string, saturation = 0) {
   return [
     ...new Array(4)
       .fill(0)
@@ -15,7 +16,11 @@ export function generateUltrablurCSS(color: string, background: string) {
         (_, i) =>
           `radial-gradient(circle farthest-side at ${config.stops[i]
             .map((s) => s + "%")
-            .join(" ")}, ${tinycolor.mix(color, background, config.mix[i])} 0%, transparent 100%)`
+            .join(" ")}, ${tinycolor.mix(
+            color,
+            background,
+            Math.min(100, Math.max(0, config.mix[i] + saturation))
+          )} 0%, transparent 100%)`
       ),
     background,
   ].join(", ");
