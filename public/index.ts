924aaadebfae865949acc6ed55c70d7d8170caee
Meow <50887230+itzTheMeow@users.noreply.github.com>
2023-01-26 10:54:12 -0500

    add intensity

--- public/index.ts
+++ public/index.ts
@@ -1,14 +1,15 @@
 import { generateUltrablurCSS } from "../dist";
 
-const [Foreground, Background, Saturation] = document.body.querySelectorAll("input");
+const [Foreground, Background, Saturation, Intensity] = document.body.querySelectorAll("input");
 
 function update() {
   document.body.style.background = generateUltrablurCSS(
     Foreground.value,
     Background.value,
-    Number(Saturation.value)
+    Number(Saturation.value),
+    Number(Intensity.value)
   );
 }
 update();
 
-Foreground.oninput = Background.oninput = Saturation.oninput = update;
+Foreground.oninput = Background.oninput = Saturation.oninput = Intensity.oninput = update;
--- src/css.ts
+++ src/css.ts
@@ -5,10 +5,16 @@ import config from "./config";
  * Generate a CSS `background` value using radial-gradient.
  * @param color The foreground color to use.
  * @param background The background color to use.
- * @param saturation Adds this value to the tinycolor mix percentage (0-100). Lower values blend toward the background.
+ * @param saturation Adds this value to the tinycolor mix percentage (0-100). Lower values blend toward the background. (default 65)
+ * @param intensity Multiply this value by the mix percentage base (1-7). Higher values create a more random feel.
  * @returns The CSS string for the `background` rule.
  */
-export function generateUltrablurCSS(color: string, background: string, saturation = 0) {
+export function generateUltrablurCSS(
+  color: string,
+  background: string,
+  saturation = 65,
+  intensity = 1
+) {
   return [
     ...new Array(4)
       .fill(0)
@@ -19,7 +25,7 @@ export function generateUltrablurCSS(color: string, background: string, saturati
             .join(" ")}, ${tinycolor.mix(
             color,
             background,
-            Math.min(100, Math.max(0, config.mix[i] + saturation))
+            Math.min(100, Math.max(0, config.mix[i] * intensity + saturation))
           )} 0%, transparent 100%)`
       ),
     background,
