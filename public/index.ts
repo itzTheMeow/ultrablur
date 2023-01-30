cc2375a0c6af0d6cb4e40c43a18607e8f3f79eea
Meow <50887230+itzTheMeow@users.noreply.github.com>
2023-01-30 16:47:54 -0500

    add code preview and remove canvas for now

--- public/index.ts
+++ public/index.ts
@@ -1,6 +1,7 @@
 import { generateUltrablurCSS } from "../dist";
 
-const [Foreground, Background, Saturation, Intensity] = document.body.querySelectorAll("input");
+const [Foreground, Background, Saturation, Intensity] = document.body.querySelectorAll("input"),
+  codePreview = document.getElementById("code")!;
 
 function update() {
   document.body.style.background = generateUltrablurCSS(
@@ -9,6 +10,7 @@ function update() {
     Number(Saturation.value),
     Number(Intensity.value)
   );
+  codePreview.innerText = `generateUltrablurCSS("${Foreground.value}", "${Background.value}", ${Saturation.value}, ${Intensity.value})`;
 }
 update();
 
